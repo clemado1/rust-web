8cfdf0d1064c7fd0d116c995f36100e1657ed9a5
clemado1 <clemado1@gmail.com>
2024-05-26 19:00:09 +0900

    add: validating with serde

--- axum/data/src/routes/mod.rs
+++ axum/data/src/routes/mod.rs
@@ -1,7 +1,10 @@
 mod hello_world;
+mod validate_with_serde;
 
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
+use validate_with_serde::validate_with_serde;
 
 pub fn create_routes() -> Router {
     Router::new().route("/hello_world", get(hello_world::hello_world))
+    .route("/validate_data", post(validate_with_serde))
 }
