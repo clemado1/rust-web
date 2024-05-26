6a8a4e45c1eaeb3c6202d6440c5f0d48957ee5a1
clemado1 <clemado1@gmail.com>
2024-05-26 19:37:43 +0900

    add: custom extractors

--- axum/data/src/routes/mod.rs
+++ axum/data/src/routes/mod.rs
@@ -1,10 +1,13 @@
+mod custom_json_extractor;
 mod hello_world;
 mod validate_with_serde;
 
 use axum::{routing::{get, post}, Router};
+use custom_json_extractor::custom_json_extractor;
 use validate_with_serde::validate_with_serde;
 
 pub fn create_routes() -> Router {
     Router::new().route("/hello_world", get(hello_world::hello_world))
     .route("/validate_data", post(validate_with_serde))
+    .route("/custom_json_extractor", post(custom_json_extractor))
 }
