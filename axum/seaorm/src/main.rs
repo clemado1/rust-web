b015331e05de4ff8a3618ea29f8d8c931069239a
clemado1 <clemado1@gmail.com>
2024-08-02 22:20:13 +0900

    add: connect database

--- axum/seaorm/src/main.rs
+++ axum/seaorm/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use dotenvy_macro::dotenv;
+
+use seaorm::run;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let database_uri = dotenv!("DATABASE_URL");
+    run(database_uri).await;
 }
