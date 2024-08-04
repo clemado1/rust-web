acae4f1f490fc5ed7e6ffc055b95ec8f818552c6
clemado1 <clemado1@gmail.com>
2024-08-04 22:36:22 +0900

    add: get one task

--- axum/data/src/routes/mod.rs
+++ axum/data/src/routes/mod.rs
@@ -1,5 +1,6 @@
 mod create_task;
 mod custom_json_extractor;
+mod get_one_task;
 mod hello_world;
 mod validate_with_serde;
 
@@ -7,16 +8,20 @@ use axum::{
     routing::{get, post},
     Extension, Router,
 };
+use sea_orm::DatabaseConnection;
+
 use create_task::create_task;
 use custom_json_extractor::custom_json_extractor;
-use sea_orm::DatabaseConnection;
+use get_one_task::get_one_task;
+use hello_world::hello_world;
 use validate_with_serde::validate_with_serde;
 
 pub fn create_routes(database: DatabaseConnection) -> Router {
     Router::new()
-        .route("/hello_world", get(hello_world::hello_world))
+        .route("/hello_world", get(hello_world))
         .route("/validate_data", post(validate_with_serde))
         .route("/custom_json_extractor", post(custom_json_extractor))
         .route("/tasks", post(create_task))
+        .route("/tasks/:task_id", get(get_one_task))
         .layer(Extension(database))
 }
