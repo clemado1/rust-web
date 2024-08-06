1f78f95c99117a3d1372726b53b613729bef2d99
clemado1 <clemado1@gmail.com>
2024-08-06 22:18:33 +0900

    add: get tasks filter

--- axum/data/src/routes/get_tasks.rs
+++ axum/data/src/routes/get_tasks.rs
@@ -1,8 +1,12 @@
-use axum::{extract::Path, http::StatusCode, Extension, Json};
-use sea_orm::{DatabaseConnection, EntityTrait};
-use serde::Serialize;
+use axum::{
+    extract::{Path, Query},
+    http::StatusCode,
+    Extension, Json,
+};
+use sea_orm::{ColumnTrait, Condition, DatabaseConnection, EntityTrait, QueryFilter};
+use serde::{Deserialize, Serialize};
 
-use crate::database::tasks::Entity as Tasks;
+use crate::database::tasks::{self, Entity as Tasks};
 
 #[derive(Serialize)]
 pub struct ResponseTask {
@@ -30,10 +34,26 @@ pub async fn get_one_task(
     }
 }
 
+#[derive(Deserialize)]
+pub struct GetTasksQueryParams {
+    priority: Option<String>,
+}
+
 pub async fn get_all_tasks(
     Extension(database): Extension<DatabaseConnection>,
+    Query(query_params): Query<GetTasksQueryParams>,
 ) -> Result<Json<Vec<ResponseTask>>, StatusCode> {
+    let mut priority_filter = Condition::all();
+    if let Some(priority) = query_params.priority {
+        priority_filter = if priority.is_empty() {
+            priority_filter.add(tasks::Column::Priority.is_null())
+        } else {
+            priority_filter.add(tasks::Column::Priority.eq(priority))
+        }
+    }
+
     let tasks = Tasks::find()
+        .filter(priority_filter)
         .all(&database)
         .await
         .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?
