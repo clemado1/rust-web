d7ce87dadc6d97658cd821a8ca34c38019ffad64
clemado1 <clemado1@gmail.com>
2024-08-16 22:46:33 +0900

    add: guarding a route

--- axum/data/src/routes/create_task.rs
+++ axum/data/src/routes/create_task.rs
@@ -1,8 +1,13 @@
-use axum::{Extension, Json};
-use sea_orm::{ActiveModelTrait, DatabaseConnection, Set};
+use axum::{http::StatusCode, Extension, Json};
+use axum_extra::{
+    headers::{authorization::Bearer, Authorization},
+    TypedHeader,
+};
+use sea_orm::{ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, Set};
 use serde::Deserialize;
 
-use crate::database::tasks;
+use crate::database::users::Entity as Users;
+use crate::database::{tasks, users};
 
 #[derive(Deserialize)]
 pub struct RequestTask {
@@ -12,17 +17,28 @@ pub struct RequestTask {
 }
 
 pub async fn create_task(
+    authorization: TypedHeader<Authorization<Bearer>>,
     Extension(database): Extension<DatabaseConnection>,
     Json(request_task): Json<RequestTask>,
-) {
+) -> Result<(), StatusCode> {
+    let token = authorization.token();
+
+    let user = Users::find()
+        .filter(users::Column::Token.eq(token))
+        .one(&database)
+        .await
+        .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?
+        .ok_or(StatusCode::UNAUTHORIZED)?;
+
     let new_task = tasks::ActiveModel {
         priority: Set(request_task.priority),
         title: Set(request_task.title),
         description: Set(request_task.description),
+        user_id: Set(Some(user.id)),
         ..Default::default()
     };
 
-    let result = new_task.save(&database).await.unwrap();
+    new_task.save(&database).await.unwrap();
 
-    dbg!(result);
+    Ok(())
 }
